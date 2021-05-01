55eacd735c2bdab793f6419fc675c4564eb83544
PedroGSouto <pedromgsouto@gmail.com>
2021-05-01 21:21:24 +0100

    Added Sha-256 support, altered login and register, added DB scripts

---
+++ Database/tables_create.sql
@@ -0,0 +1,454 @@
+CREATE SCHEMA pmate;
+GO
+
+
+
+
+------------------------------------ LOCATIONS RELATED -----------------------------------
+
+CREATE TABLE pmate.Pais( 
+	id int IDENTITY(1,1) PRIMARY KEY, 
+	nome VARCHAR(50) UNIQUE,
+);
+
+CREATE TABLE pmate.Distrito( 
+	id int IDENTITY(1,1) PRIMARY KEY, 
+	nome VARCHAR(50),
+	pais int ,
+
+	FOREIGN KEY (pais) REFERENCES pmate.Pais(id),
+	CONSTRAINT pais_distrito_unico UNIQUE (nome,pais),
+
+);
+
+CREATE TABLE pmate.Concelho(
+	id int IDENTITY(1,1) PRIMARY KEY, 
+	nome VARCHAR(50),
+	distrito  int,
+
+	FOREIGN KEY (distrito) REFERENCES pmate.Distrito(id),
+	CONSTRAINT distrito_concelho_unico UNIQUE (nome,distrito),
+);
+
+CREATE TABLE pmate.freguesia(
+	id int IDENTITY(1,1) PRIMARY KEY, 
+	nome VARCHAR(50),
+	concelho int,
+
+	FOREIGN KEY (concelho) REFERENCES pmate.Concelho(id),
+	CONSTRAINT concelho_freguesia_unico UNIQUE (nome,concelho),
+);
+
+
+
+
+
+
+------------------------------------ USER RELATED -----------------------------------
+
+CREATE TABLE pmate.Users(
+    id nvarchar(450) ,
+    Nome nvarchar(100) ,
+    --Username nvarchar(30) , tratado pelo ASP USERS
+    --Password nvarchar(20) , tratado pelo ASP USERS
+    --QuestionPass nvarchar(100) , tratado pelo ASP USERS
+    --AnswerPass nvarchar(100) , tratado pelo ASP USERS
+    --IdEscola int , tratado pelo UserRoleEscola
+    DataDeRegisto datetime ,
+    --IdTipoUser int NOT NULL, role?
+    Morada nvarchar(100) ,
+    CodPostal nvarchar(4) ,
+    ExtensaoCodPostal nvarchar(3) ,
+    Localidade nvarchar(30) ,
+    idsexo int NOT NULL,
+    idNEE int NOT NULL,
+    iduser_pk int ,
+    editado bit NOT NULL,
+    obs nvarchar(max) ,
+    -- Guid uniqueidentifier , --so usado em UMA LINHA !!!
+    -- StatusEmail bit , --so usado em UMA LINHA !!!
+    -- UU nvarchar(100) , --so usado em 49 linhas !!!
+    rgpd_comunicacao bit , --5335
+    rgpd_estatistica bit , --5335
+    rgpd_data datetime , --4335
+    validade_pessoal datetime , --4335
+    validade_estatistica datetime , --4335
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES dbo.AspNetUsers(Id)
+);
+
+
+
+
+CREATE TABLE pmate.UserContactoTipo(     -- OLD tbldicionariocontacto
+	id int IDENTITY(1,1) PRIMARY KEY,  -- OLD idDicionario
+	tipo nvarchar(50) UNIQUE,          -- OLD Descricao
+);
+
+
+
+
+CREATE TABLE pmate.UserContacto(      -- OLD tblcontacto
+	id int IDENTITY(1,1) PRIMARY KEY, -- OLD idContacto
+	idUser int NOT NULL,
+	idTipo int NOT NULL,              -- OLD: idDic
+	Descricao nvarchar(255) NOT NULL,
+	--iduser_pk int NOT NULL, WHY iduser again?
+);
+
+
+
+
+
+
+------------------------------------ SCHOOL RELATED -----------------------------------
+
+CREATE TABLE pmate.TipoEscola(
+    id_tipo_escola int NOT NULL,
+    TipoEscola char(10) ,
+    DescricaoTipoEscola char(100) ,
+    PRIMARY KEY(id_tipo_escola)
+);
+
+
+CREATE TABLE pmate.Escola(
+    id int NOT NULL,
+    IdTipoEscola int NOT NULL,
+    NomeEscola nvarchar(100) NOT NULL,
+    -- IdDistrito int NOT NULL,
+	-- IdPais int NOT NULL,
+    Morada char(100) ,
+    CodigoPostal nvarchar(4) ,
+    ExtensaoCodPostal nvarchar(3) ,
+    Localidade nvarchar(50) ,
+    Telefone nchar(9) ,
+    Fax nvarchar(9) ,
+    Email nvarchar(255) ,
+    Website nvarchar(255) , -- OLD Http
+    Idconcelho int ,
+    IdFreguesia int , --s� usado em 72 linhas das 23902, remover?
+    estado bit ,
+    ENSINOS nvarchar(30) ,
+    LATITUDE nvarchar(30) ,
+    LONGITUDE nvarchar(30) ,
+	GRUPONATUREZA nvarchar(30) ,     -- usado 2650 em 23902
+	COD_DGEEC int ,                  -- usado 2647 em 23902
+	COD_DGPGF int ,                  -- usado 2647 em 23902
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(IdTipoEscola) REFERENCES pmate.TipoEscola(id_tipo_escola),
+    --FOREIGN KEY(IdFreguesia) REFERENCES dbo.Freguesias(id_freguesia),
+    FOREIGN KEY(IdConcelho) REFERENCES pmate.Concelho(id),
+    --FOREIGN KEY(IdDistrito) REFERENCES dbo.Distritos(id_distrito),
+    --FOREIGN KEY(IdPais) REFERENCES dbo.Paises(id_pais)
+
+);
+
+CREATE TABLE pmate.EscolaInfoExtra( -- ATTRS da escola potencialmente inuteis.Se forem necessários ficaram numa tabela extra
+	IdEscola int PRIMARY KEY , 
+	DataInscricao nvarchar(30) ,     -- usado 210 linhas em 23902, remover?
+	NIF nvarchar(9) ,                -- usado 664 em 23902
+	icon nvarchar(max) ,             -- usado 479 em 23902
+	PreInscEquaMat int ,             -- usado 121 em 23902	
+	UltimaActualizacao nvarchar(30), -- usado 997 em 23902
+
+	FOREIGN KEY(IdEscola) REFERENCES pmate.Escola(id)
+)
+
+
+CREATE TABLE pmate.AnoLetivo(
+	AnoLetivo nvarchar(10) PRIMARY KEY,
+	Inicio datetime NOT NULL,
+	Fim datetime NOT NULL
+);
+
+CREATE TABLE pmate.AnoEscolar(
+	id int PRIMARY KEY,
+	Ano char(50) NOT NULL
+);
+
+CREATE TABLE pmate.UserRoleEscola (
+	id int IDENTITY(1,1) PRIMARY KEY, 
+	IdUser nvarchar(450) UNIQUE  NOT NULL,
+	IdRole nvarchar(450) NOT NULL,
+	IdEscola int NOT NULL,
+	IdResponsavel int,
+	IdAnoEscolar int,
+	AnoLetivo  nvarchar(10),
+
+	FOREIGN KEY(IdUser,IdRole) REFERENCES dbo.AspNetUserRoles(UserId,RoleId), -- NEED TO CHECK THE REAL NAME OF THESE ATTRS
+
+	FOREIGN KEY(IdEscola) REFERENCES pmate.Escola(id),
+	FOREIGN KEY(IdResponsavel) REFERENCES pmate.UserRoleEscola(id),
+	FOREIGN KEY(IdAnoEscolar) REFERENCES pmate.AnoEscolar(id),
+	FOREIGN KEY(AnoLetivo) REFERENCES pmate.AnoLetivo(AnoLetivo),
+);
+
+CREATE TABLE pmate.UserEscolaHistorico (
+	DateRegistered TIMESTAMP PRIMARY KEY,
+	IdUserRoleEscola int NOT NULL, 
+	
+	FOREIGN KEY(IdUserRoleEscola) REFERENCES pmate.UserRoleEscola(id),
+);
+
+
+
+
+
+
+
+------------------------------------ MODELS  RELATED -----------------------------------
+
+CREATE TABLE pmate.Modelo(
+    id nvarchar(15) PRIMARY KEY,
+    refCicloEnsinoID char(10) NOT NULL,
+    Expr1 char(3) NOT NULL,
+    SubTema int NOT NULL,
+    Objectivo_Principal int NOT NULL,
+    Objectivo_Secundario int NOT NULL,
+    Nivel_Dificuldade int NOT NULL,
+    contador int NOT NULL
+);
+
+
+
+
+
+
+
+------------------------------------ EXAMS  RELATED -----------------------------------
+
+CREATE TABLE pmate.Competicao(        -- OLD dbo.tblcompeticaoEvento
+	id int IDENTITY(1,1) PRIMARY KEY, -- OLD IdCompeticaoEvento
+	Nome nvarchar(max) NOT NULL, 	  -- OLD NomeEvento
+	DataInicio datetime NOT NULL,
+	DataFim datetime NULL, 			  -- OLD Datafim
+	Etiqueta nvarchar(50) NOT NULL
+) 
+
+CREATE TABLE pmate.Prova(
+	id int IDENTITY(1,1) PRIMARY KEY, -- OLD IdCompeticao
+	IdAuthor nvarchar(450) NULL, 				  -- OLD IdUser
+	-- IdModoProva int NULL           -- OLD IdModoCompeticao & De onde vem este ID?
+	IdCompeticao int NULL,            -- OLD idCompeticaoEvento
+
+	NomeProva char(60) NULL,          -- OLD NomeCompeticao
+	DataCriacao datetime NULL,
+	MaxEscolas int NULL,              -- OLD NumaMaxEqEscolas
+	MaxTentJogo int NULL,
+	TempoTotalJogo int NULL,
+	NumNiveis int NOT NULL,           -- OLD NumTotNiveis
+	VidasPorNivel int NULL,
+	NumElemsEquipa int NULL,          -- OLD NumEltosEquipa
+	Calculadora bit NULL,
+	-- VisualizarProva bit NULL, TODAS AS ROWS  a NULL -- Nunca foi usado.
+
+	DataInscFinal datetime NULL,
+	DataProva datetime NULL,          -- OLD DiaHoraCompeticao
+	InicioPreInscricao datetime NULL,
+	FimPreInscricao datetime NULL,
+	InicioInscricaoEquipas datetime NULL,
+	FimInscricaoEquipas datetime NULL,
+	FimProva datetime NULL,
+
+	Estilo nvarchar(25) NULL,
+	URL nvarchar(250) NULL,
+	TreinoVisivel bit NOT NULL,
+	RefIdCicloEnsino int NULL,
+	plataforma int NULL, 
+	-- IconName nvarchar(25) NULL, ESTA NULL EM 1870/2018 ROWS
+
+	FOREIGN KEY(IdCompeticao) REFERENCES pmate.Competicao(id),
+	FOREIGN KEY(IdAuthor)     REFERENCES pmate.Users(id),
+
+	-- Possivelmente adicionar : IdCategoria int, FOREIGN KEY(IdCategoria) REFERENCES pmate.Categoria(id),
+
+);
+
+CREATE TABLE pmate.Categoria( -- OLD dbo.tblCompeticaoBase(
+	id int IDENTITY(1,1) PRIMARY KEY, --OLD competicaoBaseId
+	nome nvarchar(50) NOT NULL, -- OLD designacao
+	-- competicaoBasePaiId int NULL, --Apenas 15/35 rows não estão Null. -- Pq q CompeticaoBase tem id para um 'BasePai'?
+	TempoTotalJogo int NULL,
+	NumTotNiveis int NOT NULL,
+	-- Calculadora bit NULL, -- Qual a utilidade de ter isto aqui se isto já é definido em cada Prova individualmente ?
+	Estilo nvarchar(25) NULL,
+	RefIdCicloEnsino int NULL,
+	IconName nvarchar(25) NULL
+)
+GO
+
+CREATE TABLE pmate.EscolaProva(
+	idEscola int NOT NULL,
+	idProva int NOT NULL
+	PRIMARY KEY(idEscola,idProva),
+	FOREIGN KEY(idEscola) REFERENCES pmate.Escola(id),
+	FOREIGN KEY(idProva) REFERENCES pmate.Prova(id)
+);
+
+CREATE TABLE pmate.Equipa(
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdProva int NOT NULL,
+	nome VARCHAR(20),
+
+	FOREIGN KEY(IdProva) REFERENCES pmate.Prova(id),
+);
+
+CREATE TABLE pmate.AlunoEquipa( -- OLD dbo.tblalunosequipas
+	-- IdAlunoEquipa int NOT NULL, provavelmente desnecessario.. + facil apanhar todos os alunos de 1 equipa ao pesquisar por IdEquipa
+	IdEquipa int,
+	IdAluno int, -- OLD IdUser
+	-- iduser_pk int NOT NULL, NO IDEA WHY AGAIN..
+	-- idequipa_pk int NOT NULL, NO IDEA WHY AGAIN..
+
+	PRIMARY KEY(IdEquipa,IdAluno),
+	FOREIGN KEY(IdEquipa) REFERENCES pmate.Equipa(id),
+	FOREIGN KEY(IdAluno)  REFERENCES pmate.UserRoleEscola(id)
+);
+
+
+
+CREATE TABLE pmate.ProvaModelo( -- OLD dbo.tblcompmodel 
+	idModelo nvarchar(15) NOT NULL,
+	idProva int NOT NULL,
+	Nivel int NOT NULL,
+
+	PRIMARY KEY(idModelo,idProva),
+	FOREIGN KEY(idModelo) REFERENCES pmate.Modelo(id),
+	FOREIGN KEY(idProva) REFERENCES pmate.Prova(id)
+);
+
+
+CREATE TABLE pmate.EnunciadoEquipa( -- OLD JogoGerado
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdProva int NOT NULL, -- OLD IdJogo
+	IdCOmpet int NOT NULL, -- Dafuk is this?
+	IdEquipa int NOT NULL,
+	idequipa_pk int NOT NULL, -- WHY AGAIN !?
+	_Data datetime NOT NULL,
+	Status int ,
+	ultimoNivel int,
+	tempo char(10),
+
+	FOREIGN KEY(IdProva) REFERENCES pmate.Prova(id),
+	FOREIGN KEY(IdEquipa) REFERENCES pmate.Equipa(id),
+)
+
+CREATE TABLE pmate.EnunciadoEquipaNivel( -- FALTAM AQUI ATTRS!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipa int,
+
+	FOREIGN KEY(IdEnunciadoEquipa) REFERENCES pmate.EnunciadoEquipa(id), 
+);
+
+CREATE TABLE pmate.EnunciadoEquipaNivelUserResposta(  -- FALTAM AQUI ATTRS!!!!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipaNivel int,
+
+	FOREIGN KEY(IdEnunciadoEquipaNivel) REFERENCES pmate.EnunciadoEquipaNivel(id),
+);
+
+CREATE TABLE pmate.EnunciadoEquipaNivelResposta(  -- FALTAM AQUI ATTRS!!!!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipaNivel int,
+
+	FOREIGN KEY(IdEnunciadoEquipaNivel) REFERENCES pmate.EnunciadoEquipaNivel(id),
+);
+
+
+
+
+
+
+
+
+
+------------------------------------ TRAINNING_TESTS  RELATED -----------------------------------
+
+CREATE TABLE pmate.Treino(
+	id int IDENTITY(1,1) PRIMARY KEY, -- OLD IdCompeticao
+	IdAuthor nvarchar(450) NULL, 				  -- OLD IdUser
+	-- IdModoProva int NULL           -- OLD IdModoCompeticao & De onde vem este ID?
+	IdCompeticao int NULL,            -- OLD idCompeticaoEvento
+
+	NomeProva char(60) NULL,          -- OLD NomeCompeticao
+	DataCriacao datetime NULL,
+	MaxEscolas int NULL,              -- OLD NumaMaxEqEscolas
+	MaxTentJogo int NULL,
+	TempoTotalJogo int NULL,
+	NumNiveis int NOT NULL,           -- OLD NumTotNiveis
+	VidasPorNivel int NULL,
+	NumElemsEquipa int NULL,          -- OLD NumEltosEquipa
+	Calculadora bit NULL,
+	-- VisualizarProva bit NULL, TODAS AS ROWS a NULL -- Nunca foi usado.
+
+	Estilo nvarchar(25) NULL,
+	URL nvarchar(250) NULL,
+	TreinoVisivel bit NOT NULL,
+	RefIdCicloEnsino int NULL,
+	plataforma int NULL, 
+	-- IconName nvarchar(25) NULL, ESTA NULL EM 1870/2018 ROWS
+
+	FOREIGN KEY(IdCompeticao) REFERENCES pmate.Competicao(id),
+	FOREIGN KEY(IdAuthor)     REFERENCES pmate.Users(id),
+
+	-- Possivelmente adicionar : IdCategoria int, FOREIGN KEY(IdCategoria) REFERENCES pmate.Categoria(id),
+);
+
+CREATE TABLE pmate.TreinoModelo( -- OLD dbo.tblcompmodel 
+	IdModelo nvarchar(15) NOT NULL,
+	IdTreino int NOT NULL,
+	Nivel int NOT NULL,
+	PRIMARY KEY(idTreino,idModelo),
+	FOREIGN KEY(idModelo) REFERENCES pmate.Modelo(id),
+	FOREIGN KEY(idTreino) REFERENCES pmate.Treino(id),
+);
+
+CREATE TABLE pmate.EnunciadoTreino( -- Antigo JogoGerado PRECISA DE MODIFICA�OES!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdProva int NOT NULL, -- Antigo IdJogo
+	IdCOmpet int NOT NULL, -- Dafuk is this?
+	IdEquipa int NOT NULL,
+	idequipa_pk int NOT NULL, -- WHY AGAIN !?
+	_Data datetime NOT NULL,
+	Status int ,
+	ultimoNivel int,
+	tempo char(10),
+
+	FOREIGN KEY(IdProva) REFERENCES pmate.Prova(id),
+	FOREIGN KEY(IdEquipa) REFERENCES pmate.Equipa(id),
+)
+
+
+
+CREATE TABLE pmate.EnunciadoTreinoNivel( -- FALTAM AQUI ATTRS!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipa int,
+
+	FOREIGN KEY(IdEnunciadoEquipa) REFERENCES pmate.EnunciadoEquipa(id), 
+);
+
+CREATE TABLE pmate.EnunciadoTreinoNivelUserResposta(  -- FALTAM AQUI ATTRS!!!!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipaNivel int,
+
+	FOREIGN KEY(IdEnunciadoEquipaNivel) REFERENCES pmate.EnunciadoEquipaNivel(id),
+);
+
+CREATE TABLE pmate.EnunciadoTreinoNivelResposta(  -- FALTAM AQUI ATTRS!!!!!!!!!!!!!!!!
+	id int IDENTITY(1,1) PRIMARY KEY,
+	IdEnunciadoEquipaNivel int,
+
+	FOREIGN KEY(IdEnunciadoEquipaNivel) REFERENCES pmate.EnunciadoEquipaNivel(id),
+);
+
+
+
+
+
+-- SELECTS 
+
+-- DROP TABLES 
+
+
---
+++ Database/tables_migrate.sql
@@ -0,0 +1,491 @@
+-- TRANSACTION TEMPLATE 
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.NewTable ON
+
+INSERT INTO pmate.NewTable(new_attrs)
+SELECT oldAttrs
+FROM dbo.OldTable
+
+SET IDENTITY_INSERT pmate.NewTable OFF
+COMMIT;
+
+
+---------------------------------------  IMPORTANTE  ----------------------------------------
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  PLANEAMENTO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! --
+
+--Tabelas mais importantes para tratar e usar no ASP NET CORE:
+    ---> AspNetUsers / AspNetRoles / AspNetUserRoles
+    ---> Users (tabela que tem os attrs extra) 
+    ---> TipoEscola / Escola / AnoLetivo / AnoEscolaridade / UserRoleEscola
+    ---> Prova / Competicao / Modelos / ProvaModelos
+
+-- OBJETIVOS ATUAIS:
+    -- INICIAIS
+    --> Listar Escolas / Users / Provas / Listar Treinos (Inicialmente sem filtros sabendo fazer 1, os outros é replicar)
+    --> Abrir uma Prova 
+    --> Criar uma competição
+    --> Criar 1 prova dentro ou fora de 1 competição 
+    --> Inscrever Escolas nas Provas/Competiçoes (DOUBT - Ao que parece na BD atual ainda não temos a tabela de EscolaProva -> Escolas inscritas numa prova..!?)
+
+    -- MAIS AVANÇADOS
+    --> Filtrar Provas por Tema e outros 
+    --> Filtrar Users por Roles
+    --> Abrir o Perfil de um User
+
+-- EM STANDBY - Podemos acrescentar isto + tarde :
+    --> tbls de localizações (pais, distrito etc) 
+    --> tbls com  + detalhes relativos á prova (Enunciados, Niveis,  respostas.. etc) 
+    --> tbls com Equipas que partipam nas provas
+
+--------------------------------------- END/IMPORTANTE ----------------------------------------
+
+
+
+
+------------------------------------ LOCATIONS RELATED ------------------------------
+
+
+
+-- Paises
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Pais ON
+
+INSERT INTO pmate.Pais(id,nome)
+SELECT IdPais,NomePais
+FROM dbo.tblpais
+
+INSERT INTO pmate.Pais (id,nome) VALUES (4,'Outros Países');
+
+SET IDENTITY_INSERT pmate.Pais OFF
+COMMIT;
+
+select * from tblpais
+select * FROM pmate.Pais order by id
+-- select * FROM [pmate-Equamat2000].[pmate].[Pais] order by id
+GO
+
+
+
+--Distritos 
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Distrito ON
+
+INSERT INTO pmate.Distrito(id,nome)
+SELECT  IdDistrito, NomeDistrito
+FROM dbo.tbldistritos
+
+UPDATE pmate.Distrito
+SET pais = 1
+WHERE id < 21;
+
+UPDATE pmate.Distrito
+SET pais = 4
+WHERE id > 20;
+
+SET IDENTITY_INSERT pmate.Distrito OFF
+COMMIT;
+
+select * from tbldistritos
+select * FROM pmate.Distrito order by id
+GO
+
+
+
+-- Concelhos
+-- 1.Bulk insert na temporary_table com as cenas do csv que tem DISTRITO - CONCELHO - FREGUESIA
+-- 2.Fazer um Join pelos nomes dos concelhos:  temp_table com tblconcelhos.. Teremos : ID, Distrito, Concelho , FREGUESIA 
+-- 3.Fazer INSERT na tabela pmate.Concelhos. id -> ID , nome -> Concelho, Distrito(FK) -> Distrito
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Concelho ON
+
+ ----- 1 -----
+CREATE TABLE ConcelhosTemp(
+   code1 varchar(300),
+   distrito varchar(300),
+   code2 varchar(300),
+   concelho varchar(300),
+   code3 varchar(300),
+   freguesia varchar(300),
+   code4 varchar(300),
+   -- some_weird_type varchar(300)
+);
+
+BULK INSERT ConcelhosTemp
+    FROM 'C:\Users\Lenovo\Desktop\PI\DistritosConcelhosFreguesias_CAOP2013_Populacao_Censos2011.csv'
+    WITH
+    (
+	CODEPAGE = 'OEM',
+	DATAFILETYPE ='char',
+	FIRSTROW = 2,          --Ignorar cabeçalho do file
+    FIELDTERMINATOR = ';',  --CSV field delimiter
+    ROWTERMINATOR = ';;',   --Use to shift the control to next row
+    ERRORFILE = 'C:\Users\Lenovo\Desktop\PI\BulkInsertErrors.csv',
+    TABLOCK )
+
+ ----- 2 & 3 -----
+INSERT INTO pmate.Concelho(id,nome,distrito)
+SELECT  idConcelho,NomeConcelho,distrito
+FROM tblconcelhos INNER JOIN ConcelhosTemp ON NomeConcelho=concelho;
+
+SET IDENTITY_INSERT pmate.Concelho OFF
+COMMIT;
+
+select * from ConcelhosTemp;
+select * from pmate.concelho;
+select * from tblconcelhos;
+
+
+
+-- Freguesias -> Estrategia igual ao dos Concelhos
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Freguesia ON
+
+SET IDENTITY_INSERT pmate.Freguesia OFF
+COMMIT;
+GO
+
+
+
+
+
+
+
+
+
+------------------------------------ USER RELATED -----------------------------------
+
+-- Select from tblUsers and AspNetUsers --
+Select * 
+from [pmate-Equamat2000].dbo.tblUsers
+where Login=':)'
+
+SELECT *
+from [pmate2-demo].dbo.AspNetUsers
+where UserName='AS'
+------------------------------------------
+
+-- Delete users from AspNetUsers --
+Delete
+from [pmate2-demo].dbo.AspNetUsers
+where Id is not NULL
+-----------------------------------
+
+-- Fix tblUsers before Migration --
+Update [pmate-Equamat2000].dbo.tblUsers
+set Nome = 'Empty'
+where Nome is null
+-----------------------------------
+
+-- Fix AspNetUsers after Migration --
+Update [pmate2-demo].dbo.AspNetUsers
+Set EmailConfirmed=1
+Set NormalizedUserName= Upper(UserName)
+Set SecurityStamp = 'abc'
+-------------------------------------
+
+-- Migrate Users from tblUsers to AspNetUsers --
+BEGIN TRANSACTION;
+
+INSERT INTO [pmate2-demo].dbo.AspNetUsers(Id,UserName,PasswordHash, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount, Age, Name, Roles, UserId) 
+SELECT IdUser,Login, [pmate2-demo].dbo.encodebase64(HASHBYTES('SHA2_512',cast(Password as varchar(max)))), 0, 0, 0, 0, 0, 0, Left(Nome,80), 0, 0
+FROM [pmate-Equamat2000].dbo.tblUsers
+
+COMMIT;
+------------------------------------------------
+
+-- Scalar-valued Function to Convert Sha512 to Base64 --
+USE [pmate2-demo]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[encodebase64]    Script Date: 01/05/2021 21:18:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[encodebase64](@hashedValue VARBINARY(8000)) 
+RETURNS NVARCHAR(MAX)
+BEGIN
+	Declare @converted NVARCHAR(MAX)
+
+	Select @converted = CAST(N'' AS XML).value(
+          'xs:base64Binary(xs:hexBinary(sql:variable("@hashedValue")))'
+        , 'NVARCHAR(MAX)'
+    )
+ Return @converted
+END 
+GO
+----------------------------------------------------------
+
+--AspNetUsers 
+--NOTA: Devido á encprytion de Passwords automatica do ASPNET CORE Talvez esta transaction não sejam aplicavel
+--NOTA: Falta confirmar os nomes dos attrs desta tbl do NetCore
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AspNetUsers ON
+
+INSERT INTO pmate.AspNetUsers(IdUser,Username,Password, QuestionPass,AnswerPass) 
+SELECT IdUser,Username,Password, QuestionPass,AnswerPass
+FROM dbo.tblUsers   
+
+SET IDENTITY_INSERT pmate.AspNetUsers OFF
+COMMIT;
+
+
+
+
+-- Users
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Users ON
+
+INSERT INTO pmate.Users(IdUser,Nome,DataDeRegisto,Morada,CodPostal,ExtensaoCodPostal,Localidade,idsexo,idNEE,iduser_pk,editado,obs,rgpd_comunicacao,rgpd_estatistica,rgpd_data,validade_pessoal,validade_estatistica) 
+SELECT IdUser,Nome,DataDeRegisto,Morada,CodPostal,ExtensaoCodPostal,Localidade,idsexo,idNEE,iduser_pk,editado,obs,rgpd_comunicacao,rgpd_estatistica,rgpd_data,validade_pessoal,validade_estatistica
+FROM dbo.tblUsers   
+
+SET IDENTITY_INSERT pmate.Users OFF
+COMMIT;
+
+
+
+
+-- UserContactoTipo
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.UserContactoTipo ON
+
+INSERT INTO pmate.UserContactoTipo(id,tipo) 
+SELECT idDicionario, Descricao
+FROM dbo.tbldicionariocontacto
+
+SET IDENTITY_INSERT pmate.UserContactoTipo OFF
+COMMIT;
+
+
+
+-- UserContacto
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.UserContacto ON
+
+INSERT INTO pmate.UserContacto(id,idUser,idTipo,Descricao) 
+SELECT idContacto,idUser,idDic,Descricao
+FROM dbo.tblcontacto   
+
+SET IDENTITY_INSERT pmate.UserContacto OFF
+COMMIT;
+
+
+
+
+
+
+------------------------------------ SCHOOL RELATED ---------------------------------
+
+--TipoEscola
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.TipoEscola ON
+
+INSERT INTO pmate.TipoEscola(id_tipo_escola, TipoEscola, DescricaoTipoEscola)
+SELECT IdTipoEscola, TipoEscola, DescricaoTipoEscola
+FROM dbo.tbltipoescola
+
+SET IDENTITY_INSERT pmate.TipoEscola OFF
+COMMIT;
+
+
+
+--Escola
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Escola ON
+
+INSERT INTO pmate.Escola(id,IdTipoEscola,NomeEscola,Morada,CodigoPostal,ExtensaoCodPostal,Localidade,Telefone,Fax,Email,Website,Idconcelho,IdFreguesia,estado,COD_DGEEC,COD_DGPGF,ENSINOS,GRUPONATUREZA,LATITUDE,LONGITUDE)
+SELECT IdEscola,IdTipoEscola, NomeEscola, Morada, CodigoPostal, ExtensaoCodPostal, Localidade, Telefone, Fax, Email, Http, refidconcelho, refIdFreguesia, estado, COD_DGEEC, COD_DGPGF, ENSINOS, GRUPONATUREZA, LATITUDE, LONGITUDE
+FROM dbo.tblescolas
+
+SET IDENTITY_INSERT pmate.Escola OFF
+COMMIT;
+
+
+
+--EscolaInfoExtra - > pode ser eventualmente necessário  caso os attrs quase n utilizados/inuteis sejam realmente pra manter
+BEGIN TRANSACTION;
+INSERT INTO pmate.EscolaInfoExtra(new_attrs)
+SELECT oldAttrs  -- NOT DONE YET
+FROM dbo.tblescolas
+COMMIT;
+
+
+
+--AnoLetivo
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AnoLetivo ON
+
+INSERT INTO pmate.AnoLetivo(AnoLetivo,Inicio,Fim)
+SELECT AnoLectivo, Inicio, Fim
+FROM dbo.dicanolectivo
+
+SET IDENTITY_INSERT pmate.AnoLetivo OFF
+COMMIT;
+
+
+
+--AnoEscolar
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AnoEscolar ON
+
+INSERT INTO pmate.AnoEscolar(id,Ano)
+SELECT idanoescolariedade, descri��o
+FROM dbo.tblDicAnoEscolaridade
+
+SET IDENTITY_INSERT pmate.AnoEscolar OFF
+COMMIT;
+
+
+
+
+-- AspNetRoles 
+-- NOTA : Fazer Insert das ROLES : 1.ADMIN, 2.PROFESSOR E 3.ALUNO
+
+
+
+--AspNetUserRoles -> ADMINS
+--NOTA: Falta confirmar os nomes dos attrs desta tbl do NetCore
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AspNetUserRole ON
+
+ALTER TABLE dbo.tblAdm 
+ADD _role int;
+
+UPDATE dbo.tblAdm
+SET _role = 1 -- should be admin's role id 
+WHERE _role is NULL
+
+INSERT INTO pmate.AspNetUserRole(IdUser,_role)
+SELECT IdUser, _role
+FROM dbo.tblAdm
+
+SET IDENTITY_INSERT pmate.AspNetUserRole OFF
+COMMIT;
+
+
+
+--AspNetUserRoles -> PROFESSORES
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AspNetUserRole ON
+
+ALTER TABLE dbo.tblprofessores 
+ADD _role int;
+
+UPDATE dbo.tblprofessores 
+SET _role = 2 -- should be professor's role id 
+WHERE _role is NULL
+
+INSERT INTO pmate.AspNetUserRole(IdUser,_role)
+SELECT IdUser, _role
+FROM dbo.tblprofessores 
+
+SET IDENTITY_INSERT pmate.AspNetUserRole OFF
+COMMIT;
+
+
+
+--AspNetUserRoles -> ALUNOS
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.AspNetUserRole ON
+
+ALTER TABLE dbo.tblalunos 
+ADD _role int;
+
+UPDATE dbo.tblalunos
+SET _role = 3 -- should be aluno's role id 
+WHERE _role is NULL
+
+INSERT INTO pmate.AspNetUserRole(IdUser,_role)
+SELECT IdUser, _role
+FROM dbo.tblalunos
+
+SET IDENTITY_INSERT pmate.AspNetUserRole OFF
+COMMIT;
+
+
+
+-- UserRoleEscola
+BEGIN TRANSACTION;
+
+INSERT INTO pmate.UserRoleEscola(IdUser,IdRole,IdEscola,IdAnoEscolar,AnoLetivo)
+SELECT IdUser, IdRole,RefIdEscola,AnoEscolaridade,RefAnoLectivo
+FROM (pmate.AspNetUserRoles 
+    INNER JOIN dbo.tblUsers ON pmate.AspNetUserRoles.IdUser = dbo.tblUsers.IdUser)
+    INNER JOIN  (select RefIdUser, max(Data),RefAnoLectivo,AnoEscolaridade from tbluserescola group by RefIdUser) ON IdUser=RefIdUser -- max->para guardar apenas a escola mais recente... as restantes podem ser guardadas na outra tabela de historico mais tarde
+
+
+COMMIT;
+
+
+
+INSERT INTO pmate.Concelho(id,nome,distrito)
+SELECT  idConcelho,NomeConcelho,distrito
+FROM tblconcelhos INNER JOIN ConcelhosTemp ON NomeConcelho=concelho;
+
+------------------------------------ MODELS  RELATED ---------------------------------
+
+--Modelo
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Modelo ON
+
+INSERT INTO pmate.Modelo(id, refCicloEnsinoID, Expr1, SubTema, Objectivo_Principal, Objectivo_Secundario, Nivel_Dificuldade, contador)
+SELECT Name, refCicloEnsinoID, Expr1, SubTema, [Objectivo Principal], [Objectivo Secund�rio], [N�vel Dificuldade], contador
+FROM dbo.rv_CodigoModelo
+
+SET IDENTITY_INSERT pmate.Modelo OFF
+COMMIT;
+
+
+
+
+
+
+
+
+------------------------------------ EXAMS  RELATED -----------------------------------
+
+--Categoria
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Categoria ON
+
+INSERT INTO pmate.Categoria(id, nome, TempoTotalJogo, NumTotNiveis, Estilo, RefIdCicloEnsino, IconName)
+SELECT competicaoBaseId, designacao, TempoTotalJogo, NumTotNiveis, Estilo, RefIdCicloEnsino, IconName
+FROM dbo.tblCompeticaoBase
+
+SET IDENTITY_INSERT pmate.Categoria OFF
+COMMIT;
+
+
+
+--Competicao
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Competicao ON
+
+INSERT INTO pmate.Competicao(id, Nome, DataInicio, DataFim, Etiqueta)
+SELECT IdCompeticaoEvento, NomeEvento, DataInicio, Datafim, Etiqueta
+FROM dbo.tblcompeticaoEvento
+
+SET IDENTITY_INSERT pmate.Competicao OFF
+COMMIT;
+
+
+
+--Prova
+BEGIN TRANSACTION;
+SET IDENTITY_INSERT pmate.Prova ON
+
+INSERT INTO pmate.Prova(id,IdAuthor, IdCompeticao, NomeProva, DataCriacao, MaxEscolas, MaxTentJogo, TempoTotalJogo, NumNiveis, VidasPorNivel, NumElemsEquipa, Calculadora, DataInscFinal, DataProva, InicioPreInscricao, FimPreInscricao, InicioInscricaoEquipas, FimInscricaoEquipas, FimProva, Estilo, URL, TreinoVisivel, RefIdCicloEnsino, plataforma)
+SELECT IdCompeticao, IdUser, idCompeticaoEvento, NomeCompeticao, DataCriacao, NumaMaxEqEscolas, MaxTentJogo, TempoTotalJogo, NumTotNiveis, VidasPorNivel, NumEltosEquipa, Calculadora, DataInscFinal, DiaHoraCompeticao, InicioPreInscricao, FimPreInscricao, InicioInscricaoEquipas, FimInscricaoEquipas, FimProva, Estilo, URL, TreinoVisivel, RefIdCicloEnsino, plataforma
+FROM dbo.tblcompeticao
+
+SET IDENTITY_INSERT pmate.Prova OFF
+COMMIT;
+
+
+------------------------------------ TRAINNING_TESTS  RELATED -----------------------------------
+
+
+
