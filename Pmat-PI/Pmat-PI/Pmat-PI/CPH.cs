d1b33d8631fded3fad245f371767350e2bed5d62
PedroGSouto <pedromgsouto@gmail.com>
2021-05-01 21:24:32 +0100

    Added Sha-256 support, altered login and register, added DB scripts

---
+++ Pmat-PI/Pmat-PI/Pmat-PI/CPH.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace Pmat_PI
+{
+    public class CPH<TUser> : IPasswordHasher<TUser> where TUser : class
+    {
+        public string HashPassword(TUser user, string password)
+        {
+            var sha512 = new SHA512Managed();
+            var bytes = UTF8Encoding.UTF8.GetBytes(password);
+            var hash = sha512.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+        {
+            if (hashedPassword == HashPassword(user, providedPassword)) {
+                return PasswordVerificationResult.Success;
+            }
+            return PasswordVerificationResult.Failed;
+        }
+    }
+}
