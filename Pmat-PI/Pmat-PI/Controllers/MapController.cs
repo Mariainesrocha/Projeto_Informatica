19c82f21bdaf4439fd42f32c912ebd1019ebd871
PedroGSouto <pedromgsouto@gmail.com>
2021-06-11 15:24:47 +0100

    Fixed the map..........

--- Pmat-PI/Pmat-PI/Controllers/MapController.cs
+++ Pmat-PI/Pmat-PI/Controllers/MapController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Pmat_PI.Models;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Pmat_PI.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using Pmat_PI.Data;
 
 namespace Pmat_PI.Controllers
 {
@@ -20,94 +23,92 @@ namespace Pmat_PI.Controllers
             _context = context;
         }
 
-        public IActionResult Map(ApplicationDbContextAlmostFinal context)
+        public IActionResult Map()
         {
             econcelhos = new Dictionary<string, ArrayList>();
-           /* List<Escola> escolas = _context.Escolas.ToList();
+            List<Escola> escolas = _context.Escolas.ToList();
+
+             foreach (Escola e in escolas) {
+                 Dictionary<string, string> attrs = new Dictionary<string, string>();
+                 ArrayList al;
+
+                 if (e.Localidade == null || e.Localidade == "") {
+                     continue;
+                 }
+
+                 if (!econcelhos.ContainsKey(e.Localidade.ToLower().Trim()))
+                 { 
+                     attrs["Nome"] =  e.NomeEscola;
+
+                     if (e.Latitude == null || e.Longitude == null)
+                     {
+                         continue;
+                     }
+                     else
+                     {
+                         attrs["Lat"] = e.Latitude;
+                         attrs["Lon"] = e.Longitude;
+                     }
+
+                     if (e.CodDgeec != null)
+                     {
+                         attrs["Dgeec"] =  e.CodDgeec.ToString();
+                     }
+                     else
+                     {
+                         attrs["Dgeec"] =  "Desconhecido";
+                     }
+
+                     if (e.CodDgpgf != null)
+                     {
+                         attrs["Dgpgf"] =  e.CodDgpgf.ToString();
+                     }
+                     else
+                     {
+                         attrs["Dgpgf"] =  "Desconhecido";
+                     }
+
+                     al = new ArrayList();
+                     al.Add(attrs);
+                     econcelhos[e.Localidade.ToLower().Trim()] = al;
+                 }
+                 else {
+                     al = econcelhos[e.Localidade.ToLower().Trim()];
+
+                     attrs["Nome"] = e.NomeEscola;
+
+                     if (e.Latitude == null || e.Longitude == null)
+                     {
+                         continue;
+                     }
+                     else
+                     {
+                         attrs["Lat"] = e.Latitude;
+                         attrs["Lon"] = e.Longitude;
+                     }
+
+                     if (e.CodDgeec != null)
+                     {
+                         attrs["Dgeec"] = e.CodDgeec.ToString();
+                     }
+                     else
+                     {
+                         attrs["Dgeec"] = "Desconhecido";
+                     }
+
+                     if (e.CodDgpgf != null)
+                     {
+                         attrs["Dgpgf"] = e.CodDgpgf.ToString();
+                     }
+                     else
+                     {
+                         attrs["Dgpgf"] = "Desconhecido";
+                     }
+
+                     al.Add(attrs);
+                 }
+             }
             
-
-
-            foreach (Escola e in escolas) {
-                Dictionary<string, string> attrs = new Dictionary<string, string>();
-                ArrayList al;
-
-                if (e.Localidade == null || e.Localidade == "") {
-                    continue;
-                }
-
-                if (!econcelhos.ContainsKey(e.Localidade.ToLower().Trim()))
-                { 
-                    attrs["Nome"] =  e.NomeEscola;
-
-                    if (e.Latitude == null || e.Longitude == null)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        attrs["Lat"] = e.Latitude;
-                        attrs["Lon"] = e.Longitude;
-                    }
-
-                    if (e.CodDgeec != null)
-                    {
-                        attrs["Dgeec"] =  e.CodDgeec.ToString();
-                    }
-                    else
-                    {
-                        attrs["Dgeec"] =  "Desconhecido";
-                    }
-
-                    if (e.CodDgpgf != null)
-                    {
-                        attrs["Dgpgf"] =  e.CodDgpgf.ToString();
-                    }
-                    else
-                    {
-                        attrs["Dgpgf"] =  "Desconhecido";
-                    }
-
-                    al = new ArrayList();
-                    al.Add(attrs);
-                    econcelhos[e.Localidade.ToLower().Trim()] = al;
-                }
-                else {
-                    al = econcelhos[e.Localidade.ToLower().Trim()];
-
-                    attrs["Nome"] = e.NomeEscola;
-
-                    if (e.Latitude == null || e.Longitude == null)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        attrs["Lat"] = e.Latitude;
-                        attrs["Lon"] = e.Longitude;
-                    }
-
-                    if (e.CodDgeec != null)
-                    {
-                        attrs["Dgeec"] = e.CodDgeec.ToString();
-                    }
-                    else
-                    {
-                        attrs["Dgeec"] = "Desconhecido";
-                    }
-
-                    if (e.CodDgpgf != null)
-                    {
-                        attrs["Dgpgf"] = e.CodDgpgf.ToString();
-                    }
-                    else
-                    {
-                        attrs["Dgpgf"] = "Desconhecido";
-                    }
-
-                    al.Add(attrs);
-                }
-            }
-           */
             ViewBag.econcelhos = econcelhos;
             return View();
         }
